b3efa0f015b62532e8893b339804d13cc9d36d40
Matt Petters <mcpetters@gmail.com>
2016-01-24 12:43:05 -0500

    implemented static templating

--- src/main/main.go
+++ src/main/main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func main() {
 	port := ":8080"
-	fmt.Printf("Go is serving on port" + port + "...")
-	http.ListenAndServe(port, http.FileServer(http.Dir("public")))
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+
+		if err == nil {
+			tmpl.Execute(w, nil)
+		}
+	})
+
+	fmt.Printf("Serving on port " + port)
+	http.ListenAndServe(port, nil)
 }
+
+const doc = `
+	<html>
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+    <h1>This is an HTML page being served by Go</h1>
+    <img src="http://pngimg.com/upload/dog_PNG2422.png" alt="" />
+  </body>
+	</html>
+`
