a75fca1a6c0b8ce36d55cf34fd4aa104f2b8911b
Matt Petters <mcpetters@gmail.com>
2016-01-24 12:33:10 -0500

    Doing the same thing with one line of code using Go's built in fileserv

--- src/main/main.go
+++ src/main/main.go
@@ -1,56 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 )
 
 func main() {
 	port := ":8080"
-	http.Handle("/", new(HTTPHandler))
 	fmt.Printf("Go is serving on port" + port + "...")
-	http.ListenAndServe(port, nil)
+	http.ListenAndServe(port, http.FileServer(http.Dir("public")))
 
-}
-
-type HTTPHandler struct {
-	http.Handler
-}
-
-func (this *HTTPHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	path := "public" + request.URL.Path
-
-	//data, err := ioutil.ReadFile(string(path))
-
-	f, err := os.Open(path)
-
-	fmt.Printf(path)
-
-	if err == nil {
-		bufferedReader := bufio.NewReader(f)
-
-		var contentType string
-
-		if strings.HasSuffix(path, ".css") {
-			contentType = "text/css"
-		} else if strings.HasSuffix(path, ".js") {
-			contentType = "application/javascript"
-		} else if strings.HasSuffix(path, ".html") {
-			contentType = "text/html"
-		} else if strings.HasSuffix(path, ".png") {
-			contentType = "image/png"
-		} else {
-			contentType = "text/plain"
-		}
-
-		writer.Header().Add("Content Type", contentType)
-		//writer.Write(data)
-		bufferedReader.WriteTo(writer)
-	} else {
-		writer.WriteHeader(404)
-		writer.Write([]byte("404 - " + http.StatusText(404)))
-	}
 }
