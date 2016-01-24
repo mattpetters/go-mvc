3873979013eac8e2de4cfcc0d3a0605cbcfdc7b7
Matt Petters <mcpetters@gmail.com>
2016-01-24 12:20:35 -0500

    Handling content types

--- src/main/main.go
+++ src/main/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func main(){
-	http.HandleFunc("/", func (w http.ResponseWriter, req *http.Request){
-		w.Write([]byte("Hello World"))
-	})
+func main() {
+	port := ":8080"
+	http.Handle("/", new(HTTPHandler))
+	fmt.Printf("Go is serving on port" + port + "...")
+	http.ListenAndServe(port, nil)
 
-	http.ListenAndServe(":8080", nil)
+}
+
+type HTTPHandler struct {
+	http.Handler
+}
+
+func (this *HTTPHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	path := "public" + request.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	fmt.Printf(path)
+
+	if err == nil {
+		var contentType string
+
+		if strings.HasSuffix(path, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else {
+			contentType = "text/plain"
+		}
+
+		writer.Header().Add("Content Type", contentType)
+		writer.Write(data)
+	} else {
+		writer.WriteHeader(404)
+		writer.Write([]byte("404 - " + http.StatusText(404)))
+	}
 }
